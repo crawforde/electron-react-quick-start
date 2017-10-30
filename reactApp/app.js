1667b0d23210660c4e448e75fb06834e3ba5457b
borajimin <borajimin@gmail.com>
2017-10-30 10:35:51 -0700

    added components

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,26 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
 /* This can check if your electron app can communicate with your backend */
-// fetch('http://localhost:3000')
-// .then(resp => resp.text())
-// .then(text => console.log(text))
-// .catch(err => {throw err})
+fetch('http://localhost:3000')
+.then(resp => resp.text())
+.then(text => console.log(text))
+.catch(err => {throw err;});
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  render() {
+    return (
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
+  }
+}
+ReactDOM.render(
+ <MyEditor />,
+ document.getElementById('root')
+);
